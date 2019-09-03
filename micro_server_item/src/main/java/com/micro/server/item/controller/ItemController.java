b442ffeb8f48ca408532999162d7710bb2b83dd9
2410620723 <2410620723@qq.com>
2019-09-03 19:46:55 +0800

    添加日志打印，以验证负载均衡

--- micro_server_item/src/main/java/com/micro/server/item/controller/ItemController.java
+++ micro_server_item/src/main/java/com/micro/server/item/controller/ItemController.java
@@ -2,7 +2,10 @@ package com.micro.server.item.controller;
 
 import com.micro.server.item.entity.Item;
 import com.micro.server.item.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/item")
 public class ItemController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ItemController.class);
+    @Value("${server.port}")
+    private String port;
     @Autowired
     private ItemService itemService;
 
     @GetMapping("/queryItemById/{id}")
     public Item queryItemById(@PathVariable Long id) {
+        LOGGER.info("server port:{}", port);
         return itemService.queryItemById(id);
     }
 }
